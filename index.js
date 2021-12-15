bed813f5cd8dae620c6ab5da170f1b6cdf8e0fef
muborizMirzoev <muboriz10@gmail.com>
2021-12-16 04:45:09 +0700

    feat: implement removeCards function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import {PaginationButton} from './js/pagination.js'
 
 const cardsElement = document.querySelector('.cards');
+let cardsItemElement = document.getElementsByClassName('cards__item'); // because it return live collection
+let cardCloseElement = document.getElementsByClassName('card__close'); // because it return live collection
+
 const baseUlr = 'http://contest.elecard.ru/frontend_data/';
 
-let cards = []
-
-fetchCards('http://contest.elecard.ru/frontend_data/catalog.json');
+let cards = [];
 
 async function fetchCards(url) {
    try {
@@ -15,36 +16,71 @@ async function fetchCards(url) {
          throw new TypeError("Ой, мы не получили JSON!");
       }
       cards = await response.json();
-      renderCards(cards)
+      console.log(cards)
+      renderCards(cards);
    } catch (error) {
       console.log(error);
    }
 }
 
+fetchCards(`${baseUlr}catalog.json`);
+
+async function sortCards(type = 'category') {
+
+   try {
+      cards = await fetchCards(`${baseUlr}catalog.json`);
+   } catch (error) {
+      console.log(error)
+   }
+
+   switch (type) {
+      case 'category':
+         cards.sort();
+         break;
+      case 'date':
+         cards.sort((a, b) => a.timestamp - b.timestamp)
+         break;
+      case 'size':
+         cards.sort((a, b) => a.filesize - b.filesize)
+         break;
+      case 'name':
+         cards.sort((a, b) => parseName(a.image) > parseName(b.image) ? 1 : -1);
+         break;
+   }
+
+   renderCards(cards)
+}
+
+// sortCards('name');
+
+
 const paginationButtons = new PaginationButton(33, 10);
 paginationButtons.render();
 paginationButtons.onChange(e => {
    console.log('paginationButtons', e.target.value);
-   cardsElement.innerHTML = ''
+   cardsElement.innerHTML = '';
    renderCards(cards, e.target.value);
 });
 
 function renderCards(cards, page = 1) {
-   const showCardsFrom = ((page - 1) * 20) + 1;
-   const showCardsTo = page * 20;
+   const showCardsFrom = ((page - 1) * 20)
+   const showCardsTo = page * 20 - 1;
 
    console.log(showCardsFrom, showCardsTo)
 
-   for (let i = showCardsFrom; i < showCardsTo; i++) {
+   for (let i = showCardsFrom; i <= showCardsTo; i++) {
       const card = cards[i];
       const name = parseName(card.image);
       const size = formatBytes(card.filesize);
       const category = capitalize(card.category);
       const date = formatDate(card.timestamp);
 
+      const deleteCards = getFromLocalStorage('deleteCards');
+      if (deleteCards.includes(String(card.timestamp))) continue
+
       cardsElement.innerHTML += `
       <div class="cards__item card">
-        <button type="button" class="card__close"><i class="fas fa-times"></i></button>
+        <button type="button" class="card__close" data-id="${card.timestamp}" ><i class="fas fa-times"></i></button>
         <img class="card__img" src="${baseUlr + card.image}" alt="">
         <ul class="card__info cards-info">
           <li class="card-info__item"><span>Name:</span> <span class="card__info-category">${name}</span></li>
@@ -55,6 +91,50 @@ function renderCards(cards, page = 1) {
       </div>`
    }
 }
+
+cardsElement.addEventListener('click', removeCards);
+
+function removeCards(event) {
+   const closeButtonElement = event.target.closest('.card__close');
+   const cardsItemElement = event.target.closest('.cards__item');
+
+   const deleteCards = getFromLocalStorage('deleteCards');
+
+   if (closeButtonElement) {
+      const id = closeButtonElement.dataset.id;
+      deleteCards.push(id);
+      setToLocalStorage('deleteCards', deleteCards)
+      cardsItemElement.remove();
+   }
+}
+
+function setToLocalStorage(key, value) {
+   localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromLocalStorage(key) {
+   return JSON.parse(localStorage.getItem(key)) || [];
+}
+
+// function updateLocalStorage() {
+//    const deleteCards = [];
+//    return function () {
+//
+//       [...cardsItemElement].forEach(card => {
+//          card.addEventListener('click', event => {
+//             const closeButton = event.target.closest('.card__close');
+//
+//             if (closeButton) {
+//                const id = closeButton.dataset.id;
+//                deleteCards.push(id);
+//                localStorage.setItem('deleteCards', JSON.stringify(deleteCards));
+//                event.currentTarget.remove();
+//             }
+//          });
+//       });
+//    }
+// }
+
 
 function parseName(str) {
    let array = str.split('/');
@@ -88,3 +168,4 @@ function formatDate(timestamp) {
 function capitalize(str) {
    return str[0].toUpperCase() + str.slice(1);
 }
+
