4da141132427e238d72ed1b52b24def8d0fe45f5
muborizMirzoev <muboriz10@gmail.com>
2021-12-21 00:31:09 +0700

    feat: implement loader class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import Filter from "./js/filter.js";
+import Filter from "./assets/js/filter.js";
+import Preloader from "./assets/js/loader.js";
 
 const popupContainerElement = document.querySelector('.popup__container');
 const resetButtonElement = document.querySelector('.reset-button');
@@ -7,6 +8,28 @@ const sortVoteElement = document.querySelector('.sort__vote');
 const searchFormElement = document.querySelector('.search__form');
 
 const filter = new Filter({cardsTypeVoteElement, sortVoteElement, resetButtonElement, searchFormElement});
+
+const initPreloaderOptions = {
+   timeout: 700,
+   closeDuration: 500,
+   template: `
+        <div class="loader">
+            <div class="loader__item" style="animation-delay: 0s"></div>
+            <div class="loader__item" style="animation-delay: .5s"></div>
+            <div class="loader__item" style="animation-delay: 1s"></div>
+        </div>
+    `
+};
+
+init(new Preloader(initPreloaderOptions));
+
+function init(preloader) {
+   preloader.open();
+   const {timeout} = preloader.options;
+   setTimeout(() => {
+      preloader.close()
+   }, timeout);
+}
 
 
 document.body.addEventListener('click', (event) => {
